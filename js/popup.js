11528f9bc1b33eb873233d5fea4d95ae64ad9b10
Paweł Uchański <uchanski.pawel@gmail.com>
2021-05-12 09:27:56 +0200

    Add login and register popups

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,37 @@
+let all = document.querySelector(".all");
+// Register
 let popup = document.querySelector(".register-popup");
 let button = document.querySelectorAll("#register");
-let exit =  document.querySelector(".exit");
+let exit =  document.querySelector(".exit-register");
 
 for(const btn of button)
 {
     btn.addEventListener("click", function(){
     popup.style.display="block";
+    all.style.filter="blur(8px)";
     });
 }
 
 exit.addEventListener("click",function(){
     popup.style.display="none";
+    all.style.filter="none";
 });
 
+
+
+// Login
+let popupLogin = document.querySelector(".login-popup");
+let buttonLogin = document.querySelector("#login");
+let exitLogin = document.querySelector(".exit-login");
+
+
+buttonLogin.addEventListener("click", function(){
+popupLogin.style.display="block";
+all.style.filter="blur(8px)";
+});
+
+exitLogin.addEventListener("click",function(){
+    popupLogin.style.display="none";
+    all.style.filter="none";
+});
 
