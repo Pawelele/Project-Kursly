d3c1f18552013d1bfd67c7c847c5b8f12027fa8f
Paweł Uchański <uchanski.pawel@gmail.com>
2021-06-09 02:17:49 +0200

    added responsivity

--- sendForm.php
+++ sendForm.php
@@ -6,8 +6,11 @@
     $header = 'Reply-To: <'.$_POST['email']."> \r\n";
     $header .= "MIME-Version: 1.0 \r\n";
     $header .= "Content-Type: text/html; charset=UTF-8";
-    $wiadomosc = "Test wysyłania wiadomości";
-    $message = "Test wysyłania wiadomości";
+    $wiadomosc = "<p>Dostałes wiadomość od:</p>";
+    $wiadomosc .= "<p>Imie i nazwisko: ".$_POST['name']. "</p>";
+    $wiadomosc .= "<p>Email: ".$_POST['email']. "</p>";
+    $wiadomosc .= "<p>Wiadomość: ".$_POST['message']."</p>";
+    $message = "<!doctype html><html><head><meta charset='UTF-8'>".$wiadomosc."</head></html>";
     $subject = 'Wiadomosc ze strony Kursly';
     $subject ='=?utf-8?B?'.base64_encode($subject).'?=';
 
